285b0d3e157ab005c5b0099c5d36166bc4af722b
Victoria Meng <nights1ststar@protonmail.com>
2018-06-12 20:23:17 +0000

    finished create method with Characters' string attributes

--- db/migrate/20180612201849_create_character.rb
+++ db/migrate/20180612201849_create_character.rb
@@ -1,4 +1,14 @@
 class CreateCharacter < ActiveRecord::Migration[5.2]
   def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :race
+      t.string :gender
+      t.string :age
+      t.string :body_type
+      t.string :skin_color
+      t.string :eye_color
+      t.string :hair_color
+    end
   end
 end
