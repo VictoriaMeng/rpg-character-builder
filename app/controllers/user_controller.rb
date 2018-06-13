7ff695309afe527b7bdff968938be95949feda9c
Victoria Meng <nights1ststar@protonmail.com>
2018-06-13 21:39:22 +0000

    added login checks to view login, signup, and user pages

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,28 @@
 class UserController < ApplicationController
 
   get '/signup' do
-    erb :'users/signup'
+    if logged_in?
+      redirect "/users/#{session[:id]}"
+    else
+      erb :'users/signup'
+    end
   end
 
   get '/users/:id' do
-    @user = User.find(params[:id])
-    erb :'users/show'
+    if logged_in? && session[:id] == params[:id].to_i
+      @user = User.find(session[:id])
+      erb :'users/show'
+    else
+      redirect "/login"
+    end
   end
 
   get '/login' do
-    erb :'users/login'
+    if logged_in?
+      redirect "/users/#{session[:id]}"
+    else
+      erb :'users/login'
+    end
   end
 
   post '/signup' do
