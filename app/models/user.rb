3ee9a56709304b19d82ad971d925ae46ae7e97ea
Victoria Meng <nights1ststar@protonmail.com>
2018-06-13 18:20:52 +0000

    corrected misspelling of 'many'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :characters
   has_many :user_games
-  has_may :games, through: :user_games
+  has_many :games, through: :user_games
 end
