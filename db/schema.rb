391ec6e52aca293f8bdd0aec2643d9b04c4c2407
Victoria Meng <nights1ststar@protonmail.com>
2018-06-13 18:34:41 +0000

    deleted all migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_13_182410) do
-
-  create_table "characters", force: :cascade do |t|
-    t.string "name"
-    t.string "race"
-    t.string "gender"
-    t.integer "user_id"
-    t.integer "game_id"
-  end
-
-  create_table "games", force: :cascade do |t|
-    t.string "name"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
