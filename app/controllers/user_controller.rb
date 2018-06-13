04a66c854f2687e8b45adc5047ff9c07aacac4f1
Victoria Meng <nights1ststar@protonmail.com>
2018-06-13 19:04:53 +0000

    created login, signup, show erb files for users, wrote basic user controller action and login/signup forms

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,14 +4,32 @@ class UserController < ApplicationController
     erb :'users/signup'
   end
 
+  get '/users/:id' do
+    erb :'users/show'
+  end
+
+  get '/login' do
+    erb :'users/login'
+  end
+
   post '/signup' do
-    binding.pry
     if blank_values?(params)
       redirect "/signup"
     else
       @user = User.create(params)
+      session[:user_id] = @user.id
+      redirect "/users/#{@user.id}"
     end
-    binding.pry
+  end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    else
+      redirect "/login"
+    end
   end
 
 
