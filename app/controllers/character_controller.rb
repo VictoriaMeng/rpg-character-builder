7fba47261ab6b82d7c0901614fcc7bf1119587a3
Victoria Meng <nights1ststar@protonmail.com>
2018-06-13 22:25:14 +0000

    control flow for new/existing games when creating characters

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -11,9 +11,19 @@ class CharacterController < ApplicationController
     if blank_values?(params[:character]) || blank_game_input?(params)
       redirect "/characters/new"
     else
-      binding.pry
       @character = Character.create(params[:character])
-      binding.pry
+      @user = User.find(session[:id])
+      @character.user_id = @user.id
+      if !params[:game_id].empty?
+        @game = Game.find(params[:game_id])
+      else
+        @game = Game.create(name: params[:new_game])
+      end
+      @character.game_id = @game.id
+      @character.save
+      @user.save
+      @game.save
+      redirect "/users/#{@user.id}"
     end
   end
 end
